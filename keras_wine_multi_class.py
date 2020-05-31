964e3eb81ee00d0b0786a7b9f4d20a1da1283394
Aayush Goel <13.june.aayush@gmail.com>
2020-05-31 07:54:33 +0530

    second commit

--- keras_wine_multi_class.py
+++ keras_wine_multi_class.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import pandas as pd
 from keras.layers import Dense
@@ -10,145 +5,41 @@ from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from keras.optimizers import Adam
 
-
-# In[2]:
-
-
 df = pd.read_csv('wines.csv')
-df
-
-
-# In[3]:
-
-
-df.info()
-
-
-# In[4]:
-
 
 y = df['Class']
 
 
-# In[5]:
-
-
-y.value_counts()
-
-
-# In[6]:
-
-
 y_cat = pd.get_dummies(y)
-
-
-# In[7]:
-
-
-y
-
-
-# In[ ]:
-
-
-
-
-
-# In[8]:
-
-
-df.columns
-
-
-# In[9]:
-
 
 X = df.drop('Class' , axis=1)
 
-
-# In[10]:
-
-
-X.info()
-
-
-# In[11]:
-
-
 model  =  Sequential()
-
-
-# In[12]:
-
-
-X.info()
-
-
-# In[13]:
-
-
-X.shape
-
-
-# In[14]:
-
-
-y_cat.shape
-
-
-# In[15]:
-
 
 # spliting the dataset
 x_train, x_test, y_train, y_test = train_test_split(X,y_cat,test_size=0.1,random_state=20)
 
 
-# In[16]:
-
-
 model.add(Dense(units=64 , input_shape=(13,), 
                 activation='relu'))
-
-
-# In[17]:
-
 
 model.add(Dense(units=3, activation='softmax'))
 
 
-# In[18]:
-
-
-model.summary()
-
-
-# In[19]:
-
+print(model.summary())
 
 model.compile(optimizer=Adam(),  
               loss='categorical_crossentropy',
              metrics=['accuracy']
              )
 
-
-# In[20]:
-
-
 ep = 20
-
-
-# In[21]:
-
 
 model.fit(x_train,y_train, epochs=ep)
 
-
-# In[23]:
-
-
 accuracy = model.evaluate(x_test, y_test, verbose=0)
 accuracy = accuracy[1]*100
-accuracy
+print(accuracy)
  
 import os
 os.system("sudo touch /root/permdata/accuracy.txt")
@@ -156,10 +47,4 @@ os.system("echo {} > /root/permdata/accuracy.txt".format(accuracy))
 
 
 model.save('/root/permdata/multiclassDL.h5')
-
-
-# In[ ]:
-
-
-
 
